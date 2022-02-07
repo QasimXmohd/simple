27ba3d38dd8c890f81cd48a9becc1df454be623f
Qasim mohd <qassimkhalifa8@gmail.com>
2022-02-07 10:30:33 +0300

    second

--- SIMPLE.java
+++ SIMPLE.java
@@ -4,7 +4,7 @@ public class SIMPLE {
 	
 	public static void main ( String ARGS[]) {
 		
-		int x=10, y=20,z;
+		int x=20, y=20,z;
 		z=x+y;
 		System.out.println("hello world this is Qasim "+ z);
 		
