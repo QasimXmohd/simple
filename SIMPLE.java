1fbfb59e6e7bcc80f69af6b04e890146e045338e
Qasim mohd <qassimkhalifa8@gmail.com>
2022-02-07 10:13:41 +0300

    first commit

---
+++ SIMPLE.java
@@ -0,0 +1,14 @@
+
+
+public class SIMPLE {
+	
+	public static void main ( String ARGS[]) {
+		
+		int x=10, y=20,z;
+		z=x+y;
+		System.out.println("hello world this is Qasim "+ z);
+		
+		
+	}
+
+}
