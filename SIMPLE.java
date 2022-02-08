cfab2185087eb097d55446dbf3c86e440f82d662
Qasim mohd <qassimkhalifa8@gmail.com>
2022-02-08 13:46:39 +0300

    four

--- SIMPLE.java
+++ SIMPLE.java
@@ -5,7 +5,7 @@ public class SIMPLE {
 	public static void main(String ARGS[]) {
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\simple\\output\\output.txt"));
-			int x = 10, y = 10, z;
+			int x = 20, y = 100, z;
 			z = x + y;
 
 			bw.write("hello world this is Qasim " + z);
