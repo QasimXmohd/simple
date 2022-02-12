07ab86c47ffdfd9f5eb653bcc73466b082bd72dc
Qasim mohd <qassimkhalifa8@gmail.com>
2022-02-12 17:01:01 +0300

    m

--- SIMPLE.java
+++ SIMPLE.java
@@ -5,7 +5,7 @@ public class SIMPLE {
 	public static void main(String ARGS[]) {
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\simple\\output\\output.txt"));
-			int x = 100, y = 100, z;
+			int x = 50, y = 100, z;
 			z = x + y;
 
 			bw.write("hello world this is Qasim \n jenkins github connection fine \n we are in year 2022 \n adition of x and y is " + z);
