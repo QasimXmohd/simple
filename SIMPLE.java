fceed70200e9fc474ca2dab738e316907f64c0cb
Qasim mohd <qassimkhalifa8@gmail.com>
2022-02-14 14:57:08 +0300

    change

--- SIMPLE.java
+++ SIMPLE.java
@@ -9,7 +9,7 @@ public class SIMPLE {
 			z = x + y;
 
 			bw.write("hello world this is Qasim \n jenkins github connection fine \n we are in year 2022 \n adition of x and y is " + z);
-			bw.write("\n gitbub triggered");
+			bw.write("\n gitbub triggered hey this is chnage ");
 			bw.close();
 		} catch (Exception ex) {
 			return;
